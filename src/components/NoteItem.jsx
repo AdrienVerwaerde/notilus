607330f3b9f9a884302578387a3f7eb56a88fe86
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2024-12-09 17:52:41 +0100

    random colors on notes

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const NoteItem = ({note}) => {
+const NoteItem = ({note, index}) => {
+    const colors = ['#4d238d', '#234f8d', '#238d61'];
+    const backgroundColor = colors[index % colors.length];
+
     return (
-        <Link to={`/edit-note/${note.id}`} className="note">
+        <Link to={`/edit-note/${note.id}`} className="note" style={{backgroundColor}}>
             <h4>{note.title.length > 50 ? (note.title.substr(0, 50)) + '...' : note.title}</h4>
             <p>{note.date}</p>
         </Link>
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -16,29 +16,35 @@ const Notes = ({ notes }) => {
             if (note.title.toLowerCase().match(text.toLocaleLowerCase())) {
                 return note;
             }
-    }))
-}
-
-useEffect(handleSearch, [text]);
-
-
-        return (
-            <section>
-                <header className="notes__header">
-                    {!showSearch && <h2>Notes</h2>}
-                    {showSearch && <input type="text" value={text} onChange={(e) => {setText(e.target.value); handleSearch();}} autoFocus placeholder="Keyword..." />}
-                    <button className='btn' onClick={() => setShowSearch(prevState => !prevState)}>{showSearch ? <MdClose /> : <CiSearch />}</button>
-                </header>
-
-                <div className="notes__container">
-                    {filteredNotes.length === 0 && <p className='empty__notes'>No notes found</p>}
-                    {
-                        filteredNotes.map(note => <NoteItem key={note.id} note={note} />)
-                    }
-                </div>
-                <Link to={'/create-note'} className='btn add__btn'><BsPlusLg /></Link>
-            </section>
-        )
+        }))
     }
 
-    export default Notes
+    const handleSearchClear = () => {
+        setText('');
+        setShowSearch(false);
+        setFilteredNotes(notes);
+    }
+
+    useEffect(handleSearch, [text]);
+
+
+    return (
+        <section>
+            <header className="notes__header">
+                {!showSearch && <h2>Notes</h2>}
+                {showSearch && <input type="text" value={text} onChange={(e) => { setText(e.target.value); handleSearch(); }} autoFocus placeholder="Keyword..." />}
+                <button className='btn' onClick={() => setShowSearch(prevState => !prevState)}>{showSearch ? <MdClose onClick={handleSearchClear} /> : <CiSearch />}</button>
+            </header>
+
+            <div className="notes__container">
+                {filteredNotes.length === 0 && <p className='empty__notes'>No notes found</p>}
+                {
+                    filteredNotes.map((note, index) => <NoteItem key={note.id} note={note} index={index} />)
+                }
+            </div>
+            <Link to={'/create-note'} className='btn add__btn'><BsPlusLg /></Link>
+        </section>
+    )
+}
+
+export default Notes
