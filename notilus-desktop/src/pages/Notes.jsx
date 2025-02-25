2ffb74fb4de9d20c1791e4c71f2fa7fef1e39b74
Adrien Verwaerde <143512438+AdrienVerwaerde@users.noreply.github.com>
2025-02-25 11:58:38 +0100

    Update Notes.jsx

--- notilus-desktop/src/pages/Notes.jsx
+++ notilus-desktop/src/pages/Notes.jsx
@@ -6,8 +6,12 @@ import NoteItem from '../components/NoteItem';
 import { MdClose } from "react-icons/md";
 import logo from "../assets/logo.png";
 
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import { SortableContext, verticalListSortingStrategy, arrayMove } from "@dnd-kit/sortable";
+import { useSensor, useSensors, PointerSensor, KeyboardSensor } from "@dnd-kit/core";
 
-const Notes = ({ notes }) => {
+
+const Notes = ({ notes, setNotes}) => {
     const [showSearch, setShowSearch] = useState(false);
     const [text, setText] = useState('');
     const [filteredNotes, setFilteredNotes] = useState(notes);
@@ -22,6 +26,25 @@ const Notes = ({ notes }) => {
 
     useEffect(handleSearch, [text]);
 
+
+// DRAG N DROP FUNCTIONNALITY //
+    const sensors = useSensors(
+        useSensor(PointerSensor, { activationConstraint: { delay: 250, tolerance: 5 } }),
+        useSensor(KeyboardSensor)
+    );
+
+    const handleDragEnd = (event) => {
+        const { active, over } = event;
+
+        if (active.id !== over.id) {
+            const oldIndex = notes.findIndex(note => note.id === active.id);
+            const newIndex = notes.findIndex(note => note.id === over.id);
+            const newNotes = arrayMove(notes, oldIndex, newIndex);
+
+            setNotes(newNotes);
+        }
+    };
+// -------------------- //
     return (
         <section>
             <header className="notes__header">
@@ -41,12 +64,17 @@ const Notes = ({ notes }) => {
                 </button>
             </header>
 
-            <div className="notes__container">
-                {filteredNotes.length === 0 && <p className='empty__notes'>No notes found</p>}
-                {
-                    filteredNotes.map((note, index) => <NoteItem key={note.id} note={note} index={index} />)
-                }
-            </div>
+            {/* Drag and Drop Context */}
+            <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+                <SortableContext items={filteredNotes.map(note => note.id)} strategy={verticalListSortingStrategy}>
+                    <div className="notes__container">
+                        {filteredNotes.length === 0 && <p className='empty__notes'>No notes found</p>}
+                        {filteredNotes.map((note, index) => (
+                            <NoteItem key={note.id} note={note} index={index} />
+                        ))}
+                    </div>
+                </SortableContext>
+            </DndContext>
             <Link to={'/create-note'} className='btn add__btn'><BsPlusLg /></Link>
         </section>
     )
