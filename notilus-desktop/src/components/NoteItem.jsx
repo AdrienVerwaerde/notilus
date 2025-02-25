fa4a4326a9909bbd0612ec56b684fe86fa060e69
Adrien Verwaerde <143512438+AdrienVerwaerde@users.noreply.github.com>
2025-02-25 11:58:07 +0100

    Update NoteItem.jsx

--- notilus-desktop/src/components/NoteItem.jsx
+++ notilus-desktop/src/components/NoteItem.jsx
@@ -1,14 +1,24 @@
+import { useSortable } from '@dnd-kit/sortable';
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { CSS } from "@dnd-kit/utilities";
 
 const NoteItem = ({note, index}) => {
     const colors = ['var(--color-primary)', 'var(--color-secondary)', 'var(--color-tertiary)', 'var(--color-quatro)'];
     const backgroundColor = colors[index % colors.length];
 
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: note.id });
+
+    const style = {
+        backgroundColor,
+        transform: CSS.Transform.toString(transform),
+        transition
+    };
+
     return (
-        <Link to={`/edit-note/${note.id}`} className="note" style={{backgroundColor}}>
+        <Link to={`/edit-note/${note.id}`} className="note" ref={setNodeRef} style={style} {...attributes} {...listeners}>
             <h4>{note.title.length > 50 ? (note.title.substr(0, 50)) + '...' : note.title}</h4>
-            <h5>{note.details.length > 80 ? (note.details.substr(0, 80)) + '...' : note.details}</h5>
+            <h5 className="note__details">{note.details.length > 80 ? (note.details.substr(0, 80)) + '...' : note.details}</h5>
             <p>{note.date}</p>
         </Link>
     )
